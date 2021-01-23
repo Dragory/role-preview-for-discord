4df9d4a2630bbc77aa36d4f00307da1a3bbe63a3
Dragory <2606411+Dragory@users.noreply.github.com>
2021-01-23 20:02:53 +0200

    Change build directory to /docs for gh-pages

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
-  ],
+  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv", "@snowpack/plugin-typescript"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
@@ -24,6 +20,6 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    /* ... */
+    out: "docs",
   },
 };
